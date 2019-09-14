39b8b1784382c3b249ff5f0135c63d8c33b11809
MarkusKos <55320353+MarkusKos@users.noreply.github.com>
2019-09-14 16:29:59 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const app = express();
+
+// разрешили запросы с других сайтов
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    fs.readFile(__dirname + "/notes.json", "utf-8", (err, data) => {
+        if (err) return res.send("Ошибка при загрузке данных");
+        // заголовок ответа сервера, который сообщает клиенту, что пришли данные в JSON и кодировки utf-8
+        res.setHeader("Content-type", "application/json;charset=utf-8");
+        // отправили данные в формате json
+        return res.send(data);
+    });
+});
+
+app.post("/",(req,res) => {
+    let data = JSON.stringify(req.body);
+    fs.writeFile(__dirname + "/notes.json", data, (err) => {
+        if(err) return res.json({status:"fail", message:"Не удалось сохранить"});
+        return res.json({status:"ok",message:"Заметки успешно сохранены"});
+    });
+});
+
+app.listen(9000, () => {
+   console.log("Сервер запущен!");
+});
